d11bcf4bdb871158c20cbf4f792063f0ca7c2406
Daniel Silverstone <dsilvers@digital-scurf.org>
2023-08-08 18:58:14 +0100

    chore: Update CLI app enough that clippy quietens down
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- pikchr-cli/src/main.rs
+++ pikchr-cli/src/main.rs
@@ -1,20 +1,28 @@
-use std::env::args;
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, Clone, Parser)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    /// Pikchr file to convert to SVG
+    pikchr: PathBuf,
+}
 
 fn main() {
-    if let Err(e) = fallible_main() {
+    let args = Args::parse();
+    if let Err(e) = fallible_main(&args) {
         eprintln!("ERROR: {}", e);
         std::process::exit(1);
     }
 }
 
-fn fallible_main() -> Result<(), Box<dyn std::error::Error>> {
-    if let Some(filename) = args().skip(1).next() {
-        let markup = std::fs::read(filename)?;
-        let markup = String::from_utf8_lossy(&markup);
-        let mut flags = pikchr::PikchrFlags::default();
-        flags.generate_plain_errors();
-        let image = pikchr::Pikchr::render(&markup, None, flags)?;
-        print!("{}", image);
-    }
+fn fallible_main(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
+    let markup = std::fs::read(&args.pikchr)?;
+    let markup = String::from_utf8_lossy(&markup);
+    let mut flags = pikchr::PikchrFlags::default();
+    flags.generate_plain_errors();
+    let image = pikchr::Pikchr::render(&markup, None, flags)?;
+    print!("{}", image);
     Ok(())
 }
